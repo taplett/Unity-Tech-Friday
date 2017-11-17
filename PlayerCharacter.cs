cf7b23e7bf8f3a28bd804e3d0921a0254ab1ddb1
ptaplett <taplett@gmail.com>
2017-11-16 20:25:09 -0600

    Add files via upload

---
+++ PlayerCharacter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerCharacter : MonoBehaviour {
+	private int _health;						//1
+
+	void Start() {								//1
+		_health = 5;							//1
+	}
+
+	public void Hurt(int damage) {				//1
+		_health -= damage;						//1
+		Debug.Log("Health: " + _health);		//1
+	}
+}
