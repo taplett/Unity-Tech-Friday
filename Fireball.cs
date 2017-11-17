f1071fc27b5fd513737a8795f3ea29505fe10ebc
ptaplett <taplett@gmail.com>
2017-11-17 07:59:31 -0600

    Update Fireball.cs

--- Fireball.cs
+++ Fireball.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
-public class Fireball : MonoBehaviour {										//1
-	public float speed = 10.0f;												//1
-	public int damage = 1;													//1
+public class Fireball : MonoBehaviour {										
+	public float speed = 10.0f;												
+	public int damage = 1;													
 
-	void Update() {															//1
-		transform.Translate(0, 0, speed * Time.deltaTime);					//1
+	void Update() {															
+		transform.Translate(0, 0, speed * Time.deltaTime);				
 	}
 
-	void OnTriggerEnter(Collider other) {									//1
-		PlayerCharacter player = other.GetComponent<PlayerCharacter>();		//1
-		if (player != null) {												//1
-			Debug.Log("Player hit");										//1
-			//player.Hurt(damage);											//2
+	void OnTriggerEnter(Collider other) {									
+		PlayerCharacter player = other.GetComponent<PlayerCharacter>();	
+		if (player != null) {												
+			Debug.Log("Player hit");										
+			//player.Hurt(damage);											
 		}
-		Destroy(this.gameObject);											//1
+		Destroy(this.gameObject);											
 	}
 }
